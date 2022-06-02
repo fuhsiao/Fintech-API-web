38c76fcaa9feee9b630165239cde1ba1cd421788
fuhsiao418 <fuhsiao418@gmail.com>
2022-06-02 12:40:23 +0800

    select options setting

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,4 @@
+from click import option
 from flask import Blueprint, render_template
 from .models import Mapping_code
 
@@ -9,8 +10,6 @@ def login():
 
 @auth.route('/sign-up')
 def sign_up():
-    # option = Mapping_code.query.filter(Mapping_code.category.in_(['op1','op2','op3'])).all()
-    option1 = Mapping_code.query.filter(Mapping_code.category == 'op1').all()
-    option2 = Mapping_code.query.filter(Mapping_code.category == 'op2').all()
-    option3 = Mapping_code.query.filter(Mapping_code.category == 'op3').all()
-    return render_template('sign-up.html', op1 = option1, op2 = option2 , op3 = option3)
+    data = Mapping_code.query.filter(Mapping_code.category.in_(['op1','op2','op3'])).all()
+    options = list(map(lambda Mapping_code:Mapping_code.serialize(),data))
+    return render_template('sign-up.html', options = options)
