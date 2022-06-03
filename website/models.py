65696598ad9c81dd9f85f7214ffdced03be660fb
fuhsiao418 <fuhsiao418@gmail.com>
2022-06-03 16:42:30 +0800

    traget backend done

--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin
+from sqlalchemy.ext.associationproxy import association_proxy
 from . import db
 
 class Users(db.Model, UserMixin):
@@ -7,15 +8,43 @@ class Users(db.Model, UserMixin):
     password = db.Column(db.String(120))
     name = db.Column(db.String(20))
     email = db.Column(db.String(45))
-    # investplan = db.relationship('Invest_plan')
+    investplan = db.relationship('Invest_plan',  backref='Users')
 
 class Invest_plan(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     plan_name = db.Column(db.String(45))
     plan_monthly_invest = db.Column(db.Integer)
     plan_time = db.Column(db.Integer)
     plan_risk = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # 預留 3 空字串(選項文字)
+    monInvest_text=''
+    planTime_text=''
+    planRisk_text=''
+
+    def __init__(self, plan_name, plan_monthly_invest, plan_time, plan_risk, monInvest_text="", planTime_text="", planRisk_text="", user_id=""):
+        self.plan_name = plan_name
+        self.plan_monthly_invest = plan_monthly_invest
+        self.plan_time = plan_time
+        self.plan_risk = plan_risk
+        self.monInvest_text = monInvest_text
+        self.planTime_text = planTime_text
+        self.planRisk_text = planRisk_text
+        self.user_id = user_id
+
+    # plam_id 序列化時在傳入(寫在__init__會變必傳參數，但 id 為 AI-PK 不用傳入)
+    def serialize(self, plan_id):
+        return{
+            "id":plan_id,
+            "planName":self.plan_name,
+            "monInvest":self.plan_monthly_invest,
+            "planTime":self.plan_time,
+            "planRisk":self.plan_risk,
+            "monInvest_text":self.monInvest_text,
+            "planTime_text":self.planTime_text,
+            "planRisk_text":self.planRisk_text
+        }
+        
 
 class Mapping_code(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from .models import Mapping_code
+from .models import Mapping_code, Invest_plan, Users
+from . import db
+import json
 
 views = Blueprint('views',__name__)
 
@@ -10,21 +12,51 @@ def Index():
     print(current_user.id)
     return render_template("index.html", user = current_user)
 
-@views.route('/target')
+
+# 投資取向 (介面)
+# ---------------------
+# options = 對應 選項文字
+# user_plans = 對應 投資計畫 欄位文字
+@views.route('/target', methods=['GET','POST'])
 @login_required
 def Target():
-    # get selector options
     opCode = Mapping_code.query.filter(Mapping_code.category.in_(['op1','op2','op3'])).all()
     options = list(map(lambda Mapping_code:Mapping_code.serialize(),opCode))
-    return render_template('target.html', user = current_user , options = options)
+    for i in current_user.investplan:
+        i.monInvest_text = mapping_codeValue(i.plan_monthly_invest, options)
+        i.planTime_text = mapping_codeValue(i.plan_time, options)
+        i.planRisk_text = mapping_codeValue(i.plan_risk, options)
+    user_plans = list(map(lambda Invest_plan:Invest_plan.serialize(Invest_plan.id),current_user.investplan))
+    return render_template('target.html', user = current_user , options = options, user_plans = user_plans )
 
-@views.route('/addnewPlan', methods=['GET','POST'])
+# 新增計畫
+@views.route('/addnewPlan', methods=['POST'])
 @login_required
 def Add_newPlan():
-    print('dsaas')
     if request.method == 'POST':
+        new_planName = request.form.get('new_planName')
+        monthlyInvest = request.form.get('monthlyInvest')
+        planTime = request.form.get('planTime')
+        planRisk = request.form.get('planRisk')
+        new_plan = Invest_plan(plan_name=new_planName, plan_monthly_invest=monthlyInvest, plan_time=planTime, plan_risk=planRisk, user_id=current_user.id)
+        db.session.add(new_plan)
+        db.session.commit()
         return redirect(url_for('views.Target'))
+    return jsonify({})
 
+# 刪除計畫
+@views.route('/deletePlan', methods=['POST'])
+@login_required
+def Delete_Plan() -> None:
+    plan_id = json.loads(request.data)
+    plan = Invest_plan.query.get(plan_id)
+    # 確認是否為 計畫主人
+    if plan.user_id == current_user.id:
+        db.session.delete(plan)
+        db.session.commit()
+    return jsonify({})
+
+    
 
 @views.route('/portfolio')
 @login_required
@@ -50,3 +82,10 @@ def Risk_assess():
 @login_required
 def Set_account():
     return render_template('account.html', user = current_user)
+
+
+def mapping_codeValue(id, options):
+    for i in options:
+        if id == i['id']:
+            return i['name']
+    return ""
